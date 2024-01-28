37c8320234571afacd37e607de21b6c902c8c149
Loukas <loukasbass@gmail.com>
2024-01-28 20:54:15 +0100

    middleware & login

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,24 @@
+import { auth, signOut } from "@/auth";
+
+
+const SettingsPages = async () => {
+    const session = await auth();
+    return ( 
+        <div>
+            {JSON.stringify(session)}
+            
+            <form action={async () => {
+                "use server"
+
+                await signOut();
+            }}>
+
+                <button type="submit">
+                    Sign out
+                </button>
+            </form>
+        </div>
+     );
+}
+ 
+export default SettingsPages;
---
+++ routes.ts
@@ -0,0 +1,24 @@
+// An array of routes that are accessible to the public
+// These routes do not reauire authentication
+// @type {string[]}
+export const publicRoutes = [
+    "/"
+]
+
+// An array of routes that are used for authentication
+// These routes will redirect legged in users to /settings
+// @type {string[]}
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register"
+]
+
+// The prefix for API authentication routes
+// Routes that start with this prefix are used for API
+// @type {string[]}
+export const apiAuthPrefix = "/api/auth";
+
+
+//The default redirect path after logging in
+//@type {string}
+export const DEFAULT_LOGIN_REDIRECT = "/settings"
