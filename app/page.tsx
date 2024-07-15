b8a3bb1c426dd54e8ac5c8720ccdf25fab5362cf
Loukas <loukasbass@gmail.com>
2024-07-15 19:06:48 +0300

    "mail address correction"

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,24 @@ import { LoginButton } from "@/components/auth/login-button";
 
 const font = Poppins({
   subsets: ["latin"],
-  weight: ["600"]
-})
+  weight: ["600"],
+});
 
 export default function Home() {
   return (
     <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       <div className="space-y-6 text-center">
-        <h1 className={cn(
-          "text-6xl font-semibold text-white drop-shadow-md",
-          font.className,
-        )}>
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
           🔐 Auth
         </h1>
-        <p className="text-white text-lg">
-          A simple authentication service
-        </p>
+        <p className="text-white text-lg">A simple authentication service</p>
         <div>
-          <LoginButton  asChild>
+          <LoginButton asChild>
             <Button variant="secondary" size="lg">
               Sign in
             </Button>
@@ -31,5 +31,5 @@ export default function Home() {
         </div>
       </div>
     </main>
-  )
+  );
 }
